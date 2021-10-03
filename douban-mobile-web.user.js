9b9922c515dac7c9fae64f00c486f3d228ee9e99
CHWJ <jwhc2012@gmail.com>
2021-10-03 18:14:31 +0800

    Update douban-mobile-web.user.js

--- douban-mobile-web.user.js
+++ douban-mobile-web.user.js
@@ -3,8 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://m.douban.com/group/*
 // @match       https://m.douban.com/movie/*
-// @grant       none
-// @version     1.0.2
+// @require     https://cdn.bootcdn.net/ajax/libs/linq.js/3.2.4/linq.min.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @version     1.0.3
 // @updateURL   https://raw.githubusercontent.com/CHWJ/New-Style-World/master/douban-mobile-web.user.js
 // @author      CHWJ
 // @description 2021/10/2 20:03:17
@@ -17,46 +20,122 @@ var BussinessObj = {
 };
 
 $(function(){
-  hideApp();
-  hideAd();
+  if(/movie\/.*/.test(window.location.href)){
+    $(".TalionNav-static").css({"margin-top":"-10px"});
+  } 
+  if(/movie\/subject\/.*\/$/.test(window.location.href)){ // 首页
+    BussinessObj.hideElements.push(".subject-banner");
+    BussinessObj.hideElements.push(".score-write");
+    BussinessObj.hideElements.push(".pics-title ~ a");
+    BussinessObj.hideElements.push(".write-comment");
+    BussinessObj.hideElements.push(".write-review");    
+    
+    // hide Ad
+    BussinessObj.hideElements.push(".card > div");
+    
+    //
+    GM_addStyle(`
+      .subject-intro {
+        padding-top: 10px;
+      }
+    `);
+  }else if(/movie\/subject\/.*\/comments/.test(window.location.href)){ // 短评
+    BussinessObj.hideElements.push(".download-app");
+  }else if(/movie\/subject\/.*\/reviews/.test(window.location.href)){ // 影评列表
+    BussinessObj.hideElements.push(".download-app");
+    BussinessObj.hideElements.push(".open-in-app-fixed-bottom");
+    
+    // todo: 最新发布的、我关注的
+  }else if(/movie\/review\//.test(window.location.href)){ // 影评详情
+    BussinessObj.hideElements.push(".download-app");
+    BussinessObj.hideElements.push('.card > section + div,.subject-card + div');
+    BussinessObj.hideElements.push('.oia-readall');
+    
+    $(".note-content").css("max-height","unset");
+    
+    // todo: 请求桌面版本的 影评详情
+  }
+  
+  if(window.location.href.indexOf("/movie/subject/")>-1){
+    if($("#celebrities").length>0){
+      // 演职员表
+      GM_addStyle(`
+      .expandActors-role {
+        color: #818181;
+        padding: 10px 0 10px 10px;
+      }
+      #expandActors .item__celebrity{
+        width: 80px;
+        margin-bottom: 10px;
+      }
+    `);
+      $("#celebrities").append($(`<div id="expandActors"></div>`));
+      $("#celebrities > header").css({"display":"flex","justify-content": "space-between"}).append(`<div><a id="btn-toggleExpandActors" href="javascript:;" style="font-size: 15px;">展开▼</a></div>`);
+      $("#btn-toggleExpandActors").on("click",toggleExpandActors);
+
+      // 替换短评
+      var timer1 = window.setInterval(function(){
+        if($(".comment-list ~ a").length>0){
+          $(".comment-list ~ a").attr("href","./comments?status=P").text("查看更多热门短评");
+          window.clearInterval(timer1);
+        }
+      },200);
+      // 替换影评
+      if($(".movie-reviews > p > a").length>0){
+        $(".movie-reviews > p > a").attr("href","./reviews").text("查看更多热门影评");
+      }
+    }
+  }
+  
   startTimer();
 });
 
-function hideApp(){
-  $(".TalionNav-static").css({"margin-top":"-25px"});
-  //BussinessObj.hideElements.push(".oia-wrap");
-  BussinessObj.hideElements.push(".subject-banner");
-  BussinessObj.hideElements.push(".score-write");
-}
-
-function hideAd(){
-  BussinessObj.hideElements.push("div.center:nth-child(7)");
-  BussinessObj.hideElements.push("body > div.page > div.card > div:nth-child(3)");
-}
-
 function startTimer(){
   BussinessObj.timerToHide = window.setInterval(function(){
-    timerToHideFunc();
+    if(BussinessObj.timerCount >= 255){
+      BussinessObj.hideElements = [];
+    }else{
+      BussinessObj.timerCount++;
+    }
+    if(BussinessObj.hideElements.length > 0){
+      var arr = [];
+      BussinessObj.hideElements.forEach(x=>{
+        var els = $(x);
+        if(els.length>0){
+          els.hide();
+        }else{
+          arr.push(x);
+        }
+      });
+      BussinessObj.hideElements = arr;
+    }else{
+      window.clearInterval(BussinessObj.timerToHide);
+      console.log("clear timerToHideFunc");
+    }
   },1000);
 }
 
-function timerToHideFunc(){
-  if(BussinessObj.timerCount >= 255){
-    BussinessObj.hideElements = [];
-  }
-  if(BussinessObj.hideElements.length > 0){
-    var arr = [];
-    BussinessObj.hideElements.forEach(x=>{
-      var els = $(x);
-      if(els.length>0){
-        els.hide();
-      }else{
-        arr.push(x);
-      }
+function toggleExpandActors(){
+  if($(".movie-celebrities").css("display")!="none"){
+    let preRole = "";
+    let roleIndex = 0;
+    $(".item__celebrity").forEach(x=>{
+        var vm = $(x).clone();
+        var role = $(vm).find(".role");
+        var roleText = $(role).text();
+        if(preRole !== roleText){
+          roleIndex++;
+          preRole = roleText;
+          $("#expandActors").append($(`<div id="role-${roleIndex}"><p class="expandActors-role">#${roleText}</p></div>`));
+        }
+        $(role).remove();
+        $("#role-"+roleIndex).append($(vm));
     });
-    BussinessObj.hideElements = arr;
+    $(".movie-celebrities").hide();
+    $("#btn-toggleExpandActors").text("展开▲");
   }else{
-    window.clearInterval(BussinessObj.timerToHide);
-    console.log("clear timerToHideFunc");
+    $(".movie-celebrities").show();
+    $("#expandActors").children().remove();
+    $("#btn-toggleExpandActors").text("展开▼");
   }
 }
